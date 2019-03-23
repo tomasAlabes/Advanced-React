87ba3b2f774c5e170da03b185656cbcfec3e32af
Tomas Alabes <tomasAlabes@users.noreply.github.com>
2019-03-22 17:15:26 -0700

    Full app running

--- sick-fits/frontend/config.js
+++ sick-fits/frontend/config.js
@@ -1,3 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
+export const prodEndpoint = `https://us1.prisma.sh/tomas-alabes/sick-fits/dev`;
 export const perPage = 4;
